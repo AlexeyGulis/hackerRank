67d352473446b41edaf453dc80cc9b54202f12c7
AlexeyGulis <kiri.dh@yandex.ru>
2022-03-15 16:04:27 +0300

    arrayManipulation

--- src/arraysTasks/Tasks.java
+++ src/arraysTasks/Tasks.java
@@ -1,14 +1,10 @@
-package easyTasks;
-
-import edu.princeton.cs.algs4.In;
+package arraysTasks;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
