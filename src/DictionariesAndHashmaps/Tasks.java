3b99c71c8f115083036a8a7e59e23ed8f70bf46c
AlexeyGulis <kiri.dh@yandex.ru>
2022-03-24 15:49:57 +0300

    checkMagazine

--- src/DictionariesAndHashmaps/Tasks.java
+++ src/DictionariesAndHashmaps/Tasks.java
@@ -1,4 +1,72 @@
 package DictionariesAndHashmaps;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
 public class Tasks {
+    public static void checkMagazine(List<String> magazine, List<String> note) {
+        // Write your code here
+        Hashtable<String, Integer> hashtable = new Hashtable<>();
+        boolean result = true;
+        for (String p : magazine
+        ) {
+            if (hashtable.containsKey(p)) {
+                hashtable.put(p, hashtable.get(p) + 1);
+            } else {
+                hashtable.put(p, 1);
+            }
+        }
+        for (String p : note
+        ) {
+            if (hashtable.containsKey(p)) {
+                if (hashtable.get(p) != 1) {
+                    hashtable.put(p, hashtable.get(p) - 1);
+                } else {
+                    hashtable.remove(p);
+                }
+            } else {
+                result = false;
+                break;
+            }
+        }
+        if (result) System.out.println("Yes");
+        else System.out.println("No");
+    }
+
+    public static String twoStrings(String s1, String s2) {
+        // Write your code here
+        return "";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s1 = bufferedReader.readLine();
+
+                String s2 = bufferedReader.readLine();
+
+                String result = twoStrings(s1, s2);
+                System.out.println(result);
+
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+    }
 }
