99c161c9cbc5f8b59cdaf985a90b7a9e05b02d29
AlexeyGulis <kiri.dh@yandex.ru>
2022-05-16 18:05:34 +0300

    substrCount init

--- src/StringManipulation/Solution.java
+++ src/StringManipulation/Solution.java
@@ -26,6 +26,87 @@ class Result {
      *  1. STRING a
      *  2. STRING b
      */
+    private static class SubstringNode {
+        private StringBuilder s = new StringBuilder();
+        private int index = 0;
+        private boolean flag = true;
+
+        public SubstringNode() {
+        }
+
+        public StringBuilder getS() {
+            return s;
+        }
+
+        public void setS(StringBuilder s) {
+            this.s = s;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+
+        public boolean isFlag() {
+            return flag;
+        }
+
+        public void setFlag(boolean flag) {
+            this.flag = flag;
+        }
+    }
+
+    public static long substrCount(int n, String s) {
+        long result = 0L;
+        SubstringNode[] substings = new SubstringNode[255];
+        for (int i = 0; i < 255; i++) {
+            substings[i] = new SubstringNode();
+        }
+        char[] string = s.toCharArray();
+        for (int i = 0; i < string.length; i++) {
+            for (int j = 0; j < substings.length; j++) {
+                StringBuilder str = substings[j].getS();
+                if (str.length() != 0 && substings[j].isFlag()) {
+                    if (substings[j].getIndex() != 0) {
+                        if (str.charAt(0) == string[i]) {
+                            str.append(string[i]);
+                            substings[j].setS(str);
+                            if ((str.length() - 1) / 2 == substings[j].getIndex()) {
+                                result++;
+                                substings[j] = new SubstringNode();
+                            }
+                        } else {
+                            substings[j].setFlag(false);
+                        }
+                    } else {
+                        if (str.charAt(str.length() - 1) != string[i]) {
+                            str.append(string[i]);
+                            result++;
+                        } else {
+                            str.append(string[i]);
+                            substings[j].setIndex(str.length() - 1);
+                        }
+                        substings[j].setS(str);
+                    }
+                }
+            }
+            if (substings[string[i]].getS().length() == 0) {
+                substings[string[i]].setS(new StringBuilder().append(string[i]));
+            }else{
+                if(!substings[string[i]].isFlag()){
+                    substings[string[i]] = new SubstringNode();
+                    substings[string[i]].setS(new StringBuilder().append(string[i]));
+                }
+            }
+
+        }
+        //result += n;
+        return result;
+    }
+
     public static String isValid(String s) {
         HashMap<Character, Integer> countLetters = new HashMap<>();
         char[] charS = s.toCharArray();
@@ -51,13 +132,13 @@ class Result {
                 minLetter = ascii.getKey();
             }
             if (max - ascii.getValue() > 1) {
-                if(ascii.getValue() == 1){
+                if (ascii.getValue() == 1) {
                     if (flag) {
                         flag = false;
                     } else {
                         return "NO";
                     }
-                }else{
+                } else {
                     return "NO";
                 }
             }
@@ -120,16 +201,24 @@ class Result {
 }
 
 public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        String s = bufferedReader.readLine();
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        String result = Result.isValid(s);
+        String s = scanner.nextLine();
+
+        long result = Result.substrCount(n, s);
 
         System.out.println(result);
 
         bufferedReader.close();
+        scanner.close();
 
     }
 }
