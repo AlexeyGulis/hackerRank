2e986ca5ebdcc528238d3fe272c7a916b7e2703f
AlexeyGulis <kiri.dh@yandex.ru>
2022-05-27 16:53:43 +0300

    Hash Tables: Ice Cream Parlor large solution

--- src/Search/Solution.java
+++ src/Search/Solution.java
@@ -1,5 +1,7 @@
 package Search;
 
+import edu.princeton.cs.algs4.In;
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
@@ -16,7 +18,36 @@ import static java.util.stream.Collectors.toList;
 class Result {
     public static void whatFlavors(List<Integer> cost, int money) {
         // Write your code here
+        HashMap<Integer, ArrayList<Integer>> table = new HashMap<>();
+        for (int i = 0; i < cost.size(); i++) {
+            ArrayList<Integer> arrayList = new ArrayList<>();
+            if (table.containsKey(cost.get(i))) {
+                arrayList = table.get(cost.get(i));
+                arrayList.add(i + 1);
+                table.put(cost.get(i), arrayList);
+            } else {
+                arrayList.add(i + 1);
+                table.put(cost.get(i), arrayList);
+            }
 
+        }
+        int f = 0;
+        int s = 0;
+        for (Map.Entry<Integer, ArrayList<Integer>> entry : table.entrySet()
+        ) {
+            if (entry.getKey() + entry.getKey() == money && entry.getValue().size() > 1) {
+                f = entry.getValue().get(0) > entry.getValue().get(1) ? entry.getValue().get(1) : entry.getValue().get(0);
+                s = entry.getValue().get(0) > entry.getValue().get(1) ? entry.getValue().get(0) : entry.getValue().get(1);
+                System.out.println(f + " " + s);
+                break;
+            }
+            if (table.containsKey(money - entry.getKey())) {
+                f = entry.getValue().get(0) > table.get(money - entry.getKey()).get(0) ? table.get(money - entry.getKey()).get(0) : entry.getValue().get(0);
+                s = entry.getValue().get(0) > table.get(money - entry.getKey()).get(0) ? entry.getValue().get(0) : table.get(money - entry.getKey()).get(0);
+                System.out.println(f + " " + s);
+                break;
+            }
+        }
     }
 }
 
