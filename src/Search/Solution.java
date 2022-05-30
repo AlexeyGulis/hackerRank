eb2fe60da53847de64814df788b889fe2d29a957
AlexeyGulis <kiri.dh@yandex.ru>
2022-05-30 16:57:20 +0300

    Binary Tree

--- src/Search/Solution.java
+++ src/Search/Solution.java
@@ -1,5 +1,6 @@
 package Search;
 
+import com.sun.source.tree.BinaryTree;
 import edu.princeton.cs.algs4.In;
 
 import java.io.*;
@@ -16,6 +17,66 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 class Result {
+
+    static class Node {
+        public Node left;
+        public Node right;
+        public int value;
+        public int depth;
+
+        public Node(Node left, Node right, int value, int depth) {
+            this.depth = depth;
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+
+        public void swap() {
+            Node temp = left;
+            left = right;
+            right = temp;
+        }
+    }
+
+    public static void inorder_print(Node root, int K){
+        if (root != null){
+            if(root.depth % K == 0){
+                root.swap();
+            }
+            inorder_print(root.left, K);
+            System.out.print(root.value+" ");
+            inorder_print(root.right, K);
+        }
+    }
+
+    public static void print_nodes(Node root, int K){
+        inorder_print(root, K);
+        System.out.println();
+    }
+
+    public static List<List<Integer>> swapNodes(List<List<Integer>> indexes, List<Integer> queries) {
+        // Write your code here
+        Queue<Node> binaryQueue = new LinkedList<>();
+
+        Node head = new Node(null, null, 1, 1);
+        binaryQueue.add(head);
+        for (int i = 0; i < indexes.size(); i++) {
+            Node temp = binaryQueue.poll();
+            if (temp != null) {
+                temp.left = (indexes.get(i).get(0) == -1) ? null : new Node(null, null, indexes.get(i).get(0), temp.depth + 1);
+                temp.right = (indexes.get(i).get(1) == -1) ? null : new Node(null, null, indexes.get(i).get(1), temp.depth + 1);
+                binaryQueue.add(temp.left);
+                binaryQueue.add(temp.right);
+            } else {
+                i--;
+            }
+        }
+        for (int i = 0; i < queries.size(); i++) {
+            print_nodes(head, queries.get(i));
+        }
+        return null;
+    }
+
     public static void whatFlavors(List<Integer> cost, int money) {
         // Write your code here
         HashMap<Integer, Integer> tM = new HashMap<>();
@@ -33,23 +94,36 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int t = Integer.parseInt(bufferedReader.readLine().trim());
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
 
-        IntStream.range(0, t).forEach(tItr -> {
+        List<List<Integer>> indexes = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
             try {
-                int money = Integer.parseInt(bufferedReader.readLine().trim());
-
-                int n = Integer.parseInt(bufferedReader.readLine().trim());
-
-                List<Integer> cost = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                        .map(Integer::parseInt)
-                        .collect(toList());
-
-                Result.whatFlavors(cost, money);
+                indexes.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
         });
+
+        int queriesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> queries = IntStream.range(0, queriesCount).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine().replaceAll("\\s+$", "");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<List<Integer>> result = Result.swapNodes(indexes, queries);
 
         bufferedReader.close();
     }
