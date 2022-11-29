4f44e5ebfb45383bfc684bd3adfc9e2a754f2956
AlexeyGulis <kiri.dh@yandex.ru>
2022-11-29 15:04:26 +0300

    Candies time O(n) space O(1)

--- src/DynamicProgramming/Solution.java
+++ src/DynamicProgramming/Solution.java
@@ -20,6 +20,51 @@ class Result {
      * The function is expected to return an INTEGER.
      * The function accepts INTEGER_ARRAY arr as parameter.
      */
+    public static long candies(int n, List<Integer> arr) {
+        long ans = 0L;
+        long candiesPerChild = 0L;
+        boolean decrease = false;
+        int lastGrowIndex = 0;
+        long mark = 0L;
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                candiesPerChild++;
+                mark = candiesPerChild;
+                lastGrowIndex = i;
+            } else {
+                if (arr.get(i - 1) < arr.get(i)) {
+                    lastGrowIndex = i;
+                    candiesPerChild++;
+                    mark = candiesPerChild;
+                    decrease = false;
+                } else if (arr.get(i - 1) > arr.get(i)) {
+                    if (!decrease) {
+                        if (candiesPerChild == 1) {
+                            mark++;
+                            ans++;
+                        } else {
+                            candiesPerChild = 1;
+                        }
+                        decrease = true;
+                    } else {
+                        if (i - lastGrowIndex == mark) {
+                            ans += i - lastGrowIndex;
+                            mark++;
+                        } else {
+                            ans += i - lastGrowIndex - 1;
+                        }
+                    }
+                } else {
+                    candiesPerChild = 1;
+                    lastGrowIndex = i;
+                    mark = candiesPerChild;
+                    decrease = false;
+                }
+            }
+            ans += candiesPerChild;
+        }
+        return ans;
+    }
 
     public static int maxSubsetSum(List<Integer> arr) {
         if (arr.size() == 0) return 0;
@@ -42,12 +87,12 @@ class Result {
         boolean[][] f = new boolean[c.length + 1][d.length + 1];
         f[0][0] = true;
         for (int i = 1; i < f.length; i++) {
-            f[i][0] = f[i-1][0] && Character.isLowerCase(c[i-1]);
+            f[i][0] = f[i - 1][0] && Character.isLowerCase(c[i - 1]);
         }
         for (int i = 1; i < c.length + 1; i++) {
             for (int j = 1; j < d.length + 1; j++) {
-                f[i][j] = (f[i-1][j-1] && Character.toUpperCase(c[i-1]) == d[j-1]) ||
-                        (f[i-1][j] && Character.isLowerCase(c[i-1]));
+                f[i][j] = (f[i - 1][j - 1] && Character.toUpperCase(c[i - 1]) == d[j - 1]) ||
+                        (f[i - 1][j] && Character.isLowerCase(c[i - 1]));
             }
         }
         return f[a.length()][b.length()] ? "YES" : "NO";
@@ -59,21 +104,22 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int q = Integer.parseInt(bufferedReader.readLine().trim());
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
 
-        IntStream.range(0, q).forEach(qItr -> {
-            try {
-                String a = bufferedReader.readLine();
+        List<Integer> arr = IntStream.range(0, n).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine().replaceAll("\\s+$", "");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
 
-                String b = bufferedReader.readLine();
+        long result = Result.candies(n, arr);
 
-                String result = Result.abbreviation(a, b);
-
-                System.out.println(result);
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        });
+        System.out.println(result);
 
         bufferedReader.close();
     }
